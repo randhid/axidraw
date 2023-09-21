ced8c02c1a24a53d4563a4705aa334b29d700b83
randhid <rand@viam.com>
2023-09-20 20:58:17 -0400

    Lint, yay

--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
-
+"""
+This module registers and intergrates EvilMadScientist's Axidraw with Viam 
+as a gantry component so you can control and code the plotter for all your fun 
+art projects.
+"""
 import asyncio
 
 from viam.resource.registry import Registry, ResourceCreatorRegistration
-from my_axidraw import AxiDraw
-
 from viam.components.gantry import Gantry
 from viam.module.module import Module
 
+from .my_axidraw import AxiDraw
+
 async def main():
     """This function creates and starts a new module, after adding all desired resources.
-    Resources must be pre-registered. For an example, see the `__init__.py` file.
-    Args:
-        address (str): The address to serve the module on
+    Resources must be pre-registered.
     """
     Registry.register_resource_creator(
-        Gantry.SUBTYPE, 
-        AxiDraw.MODEL, 
-        ResourceCreatorRegistration(AxiDraw.new_axidraw, AxiDraw.validate_config))
-    
+        Gantry.SUBTYPE,
+        AxiDraw.MODEL,
+        ResourceCreatorRegistration(AxiDraw.new_axidraw, AxiDraw.validate_config)
+        )
     module = Module.from_args()
     module.add_model_from_registry(Gantry.SUBTYPE, AxiDraw.MODEL)
     await module.start()
--- src/my_axidraw.py
+++ src/my_axidraw.py
@@ -1,7 +1,10 @@
 # viamaxidraw.py
+"""
+This contains the implementation of the AxiDraw class for initilization, validation,
+reconfiguration, and control and querying of state using viam's Gantry API methods.
+"""
 
 from typing import Any, Dict, List, Optional, Sequence, Mapping, ClassVar
-from typing_extensions import Self
 
 from pyaxidraw import axidraw
 
@@ -13,51 +16,63 @@ from viam.module.types import Reconfigurable
 from viam.proto.common import ResourceName
 from viam.resource.base import ResourceBase
 from viam.proto.common import Geometry
-
 from viam.logging import getLogger
 
 LOGGER = getLogger(__name__)
 
 
 class AxiDraw(Gantry, Reconfigurable):
+    """
+    An AxiDraw Gantry component that connects to the controller via a usb connection and
+    sets the starting positions of the plotter to 0,0,0.
+    The lengths correspond to the AxiDrawV3  lengths and the servo is assumed 
+    """
     MODEL: ClassVar[Model] = Model(ModelFamily("viam", "eggbot"), "axidraw")
     def __init__(self, name: str):
         super().__init__(name)
-       
-        # Starting 
+        # Starting State
         self.lengths = [430.0 ,291.0,17.0]
         self.position = [0.0 ,0.0,0.0]
-        self.servoUp = False
-
-
         self.is_stopped = True
-        self.ad = axidraw.AxiDraw()  
-        # Initialize class
-        self.ad.interactive()                # Enter interactive context
-        if not self.ad.connect():            # Open serial port to AxiDraw;
-            quit()                      #   Exit, if no connection.
+        self.axi_draw = axidraw.AxiDraw()
+        # Initialize class from eggbot's axidraw package
+        self.axi_draw.interactive()       # Enter interactive context
+        if not self.axi_draw.connect():   # Open serial port to AxiDraw;
+            quit()                  # Exit, if no connection.
 
     def __del__(self):
-        self.ad.disconnect()
+        self.axi_draw.disconnect()
 
     # Constructor
     @classmethod
-    def new_axidraw(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
-        axidraw = cls(config.name)
-        axidraw.reconfigure(config, dependencies)
-        return axidraw
+    def new_axidraw(
+        cls, config: ComponentConfig,
+        dependencies: Mapping[ResourceName, ResourceBase]
+        ):
+        """
+        new_axidraw intializes the Axidraw class with the default state and
+
+        """
+        my_axidraw = cls(config.name)
+        my_axidraw.reconfigure(config, dependencies)
+        return my_axidraw
 
     # Validates JSON Configuration
     @classmethod
     def validate_config(cls, config: ComponentConfig) -> Sequence[str]:
-        pass
+        """ does nothing, there are no attributes to change in this model """
 
 
     # Handles attribute reconfiguration
-    def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
-        pass
+    def reconfigure(
+        self,
+        config: ComponentConfig,
+        dependencies: Mapping[ResourceName, ResourceBase]
+        ):
+        """ reconfigure does nothing, there are no attributes to change in this model"""
 
     async def get_position(self, extra: Optional[Dict[str, Any]] = None, **kwargs) -> List[float]:
+        """ gets the current position of the axidraw  """
         return self.position
 
     @run_with_operation
@@ -68,17 +83,15 @@ class AxiDraw(Gantry, Reconfigurable):
         extra: Optional[Dict[str, Any]] = None,
         **kwargs,
     ):
+        """ moves the axidraw axes and servo to draw  """
         operation = self.get_operation(kwargs)
-
         self.is_stopped = False
         self.position = positions
 
-        if positions[2] > 0:
-            self.ad.moveto(positions[0], positions[1])
+        if positions[2] > 0: # zero is the ground plane for the third axis - the servo
+            self.axi_draw.moveto(positions[0], positions[1]) # Move with pen up
         else:
-            self.ad.lineto(positions[0], positions[1])
-
-
+            self.axi_draw.lineto(positions[0], positions[1]) # Move with pen down
 
         # Check if the operation is cancelled and, if it is, stop the gantry's motion
         if await operation.is_cancelled():
@@ -86,21 +99,28 @@ class AxiDraw(Gantry, Reconfigurable):
 
 
     async def get_lengths(self, extra: Optional[Dict[str, Any]] = None, **kwargs) -> List[float]:
+        """ returns the lengths of the axidraw axes """
         return self.lengths
 
 
     async def stop(self, extra: Optional[Dict[str, Any]] = None, **kwargs):
+        """ stops the axidraw """
         self.is_stopped = True
-        
+
     async def home(self, extra: Optional[Dict[str, Any]] = None, **kwargs):
+        """ sets the current position to 0,0,0 """
         self.position = [0,0,0]
 
     async def is_moving(self) -> bool:
+        """ queries whether the axidraw is moving """
         return not self.is_stopped
-    
-    async def get_geometries(self, extra: Optional[Dict[str, Any]] = None, **kwargs) -> List[Geometry]:
-        pass
+
+    async def get_geometries(
+        self,
+        extra: Optional[Dict[str, Any]] = None,
+        **kwargs) -> List[Geometry]:
+        """ returns nothing - no geometries associated with axidraw """
 
     async def close(self):
-        self.ad.disconnect()
-
+        """ disconnects the axidraw """
+        self.axi_draw.disconnect()
